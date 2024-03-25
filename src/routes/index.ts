de91c089e247a9e48bc62f780e26b7a8be8b9483
Hoàng Trang <success010280@gmail.com>
2024-03-25 10:18:27 +0700

    Add about us page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import SigninForm from '~/_auth/forms/SigninForm';
 import SignupForm from '~/_auth/forms/SignupForm';
 import { Home } from '~/_root/pages/Home';
-import { HeaderOnly } from '~/components/Layout';
+import { About, HeaderOnly } from '~/components/Layout';
 
 interface RouteType {
     path: string;
@@ -12,6 +12,7 @@ export const publicRoutes: RouteType[] = [
     { path: '/', component: Home },
     { path: '/sign-in', component: SigninForm, layout: null },
     { path: '/sign-up', component: SignupForm, layout: HeaderOnly },
+    { path: '/about', component: About}
 ];
 
 //export const privateRoutes = [];
